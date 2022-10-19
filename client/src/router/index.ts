9ec70af2662e868795cdc6444a5172f5da7d4105
KyleWendholt <101617205+KyleWendholt@users.noreply.github.com>
2022-10-19 15:21:10 -0400

    added routing

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,23 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/workouts",
+      name: "workouts",
+      component: () => import("../views/WorkoutsView.vue"),
+    },
+    {
+      path: "/friends",
+      name: "friends",
+      component: () => import("../views/FriendsView.vue"),
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: () => import("../views/ProfileView.vue"),
+    },
+    {
+      path: "/admin",
+      name: "admin",
+      component: () => import("../views/AdminView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
