64e61bb887cd63dcc9e3e4dc689f2b3287598d37
KyleWendholt <101617205+KyleWendholt@users.noreply.github.com>
2022-10-20 23:25:57 -0400

    cleaned up

--- client/src/components/LoginBadge.vue
+++ client/src/components/LoginBadge.vue
@@ -6,29 +6,34 @@ let isActive = ref(false);
 </script>
 
 <template>
-  <div class="buttons" v-if="session.user == null">
+  <div class="buttons navbar-item" v-if="session.user == null">
     <a class="button is-primary">
       <strong>Sign up</strong>
     </a>
     <div class="dropdown" :class="{ 'is-active': isActive }">
       <div class="dropdown-trigger">
-        <a class="button is-light" @click="isActive = !isActive">
-          Log in
-        </a>
+        <a class="button is-light" @click="isActive = !isActive"> Log in </a>
       </div>
       <div class="dropdown-menu is-active" id="dropdown-menu" role="menu">
-        <div  @click="isActive = !isActive" class="dropdown-content">
-          <a v-for="user in Users" class="dropdown-item" @click="login(user.username)"> {{user.firstname}} {{user.lastname}} </a>
+        <div @click="isActive = !isActive" class="dropdown-content">
+          <a
+            v-for="user in Users"
+            class="dropdown-item"
+            @click="login(user.username)"
+          >
+            {{ user.firstname }} {{ user.lastname }}
+          </a>
         </div>
       </div>
     </div>
   </div>
-  <div v-else>
-    Welcome {{ session.user.firstname }} {{ session.user.lastname }} (<a
-      @click="logout()"
-    >
-      Log out </a
-    >)
+  <div class="navbar-item" v-else>
+    <div class="navbar-item">
+      <div>
+        Welcome {{ session.user.firstname }} {{ session.user.lastname }}
+      </div>
+    </div>
+    <a class="button is-blue navbar-item" @click="logout()"> Log out </a>
   </div>
 </template>
 
