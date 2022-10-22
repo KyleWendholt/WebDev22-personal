bef1c5bb717a5885ca2624aebae9efa1558ccf3a
KyleWendholt <101617205+KyleWendholt@users.noreply.github.com>
2022-10-21 21:17:56 -0400

    rename exercises

--- client/src/components/Exercise.vue
+++ client/src/components/Exercise.vue
@@ -1,40 +1,45 @@
 <template>
-  <div class="card-content box container" v-for="workout in props.workouts">
+  <div class="card-content box">
     <div class="media">
       <div class="media-left">
         <figure class="image is-48x48">
           <img
             class="is-rounded"
-            :src="workout.user.profilePic"
+            :src="props.workout?.user.profilePic"
             alt="Profile image"
           />
         </figure>
       </div>
       <div class="media-content">
         <p class="title is-4">
-          {{ workout.user.firstname }} {{ workout.user.lastname }}
+          {{ props.workout?.user.firstname }} {{ props.workout?.user.lastname }}
         </p>
-        <p class="subtitle is-6">@{{ workout.user.username }}</p>
+        <p class="subtitle is-6">@{{ props.workout?.user.username }}</p>
       </div>
     </div>
 
     <div class="content">
-      {{ workout.content }}
+      {{ props.workout?.content }}
       <br />
-      {{ workout.time }}
+      {{ props.workout?.time }}
     </div>
     <footer v-if="edit" class="card-footer">
-      <a class="card-footer-item">Edit</a>
-      <a @click="removeWorkout(workout)" class="card-footer-item">Delete</a>
+      <a
+        @click="
+          removeWorkout(props.workout!);
+        "
+        class="card-footer-item"
+        >Delete</a
+      >
     </footer>
   </div>
 </template>
 
 <script setup lang="ts">
-import { removeWorkout } from '../stores/workouts';
+import { removeWorkout, Workout } from "../stores/workouts";
 
 const props = defineProps({
-  workouts: Array(),
+  workout: Workout,
   edit: Boolean,
 });
 </script>
@@ -43,7 +48,10 @@ const props = defineProps({
 .content {
   margin: 10px 0px;
 }
-.card-footer{
+.card-footer {
   height: 15px;
+}
+.box{
+  margin-top: 15px;
 }
 </style>
--- client/src/views/ExercisesView.vue
+++ client/src/views/ExercisesView.vue
@@ -1,25 +1,87 @@
 <template>
   <div class="container is-max-desktop">
-    <div class="button is-large is-fullwidth is-rounded is-link" @click="">Add workout</div>
-      <Workouts v-bind = "data"/>
+    <div class="modal" :class="{ 'is-active': modalActive }">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="field">
+          <label class="label">Message</label>
+          <div class="control">
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">New Workout</p>
+                <div class="card-header-icon">
+                  <button
+                    @click="
+                      modalActive = !modalActive;
+                      inputText = '';
+                    "
+                    class="delete icon is-large is-active"
+                  ></button>
+                </div>
+              </header>
+              <input
+                class="input card-content"
+                placeholder="Workout Description"
+                type="text"
+                v-model="inputText"
+              />
+            </div>
+          </div>
+          <button
+            @click="
+              modalActive = !modalActive;
+              addWorkout(session.user!, prettyDate(new Date()), inputText);
+              inputText = '';
+            "
+            type="submit"
+            class="button is-large is-fullwidth is-rounded is-link"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+    <div
+      class="button is-large is-fullwidth is-rounded is-link"
+      @click="modalActive = !modalActive"
+    >
+      Add workout
+    </div>
+    <div v-for="workout in Workouts">
+      <Exercises
+        v-if="workout.user == session.user"
+        :edit="true"
+        :workout="workout"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import Workouts from "../components/Exercises.vue"
-import { usersWorkouts } from "../stores/workouts";
+import Exercises from "../components/Exercise.vue";
+import Workouts, { addWorkout} from "../stores/workouts";
 import session from "../stores/session";
+import { ref } from "vue";
 
-const data= {
-  workouts: usersWorkouts(session.user!),
-  edit: true
+function prettyDate(date: Date) {
+  return (
+    (date.getHours() % 12) +
+    ":" +
+    date.getMinutes() +
+    " " +
+    (date.getMonth()+1) +
+    "/" +
+    date.getUTCDate() +
+    "/" +
+    date.getFullYear()
+  );
 }
-
+const inputText = "";
+let modalActive = ref(false);
 </script>
 
 <style scoped>
-.button{
+.button {
   margin: 15px 0px;
 }
-
 </style>
